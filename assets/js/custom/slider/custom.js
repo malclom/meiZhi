7a3efed1fa1fa01d6ba3c6dc44711730994cc793
Eumenides <zsh159456jk@163.com>
2018-02-13 14:51:51 +0800

    1
    1

--- assets/js/custom/slider/custom.js
+++ assets/js/custom/slider/custom.js
@@ -8,32 +8,42 @@
 
     $(document).ready(function () {
         
-            var $boyCounter = $(".counter");
-            if ($boyCounter.length) {
+        $.backstretch([
+            '../img/bg1.jpg'
+            , '../img/bg2.jpg'
+            , '../img/bg3.jpg'
+            , '../img/bg4.jpg'
+            , '../img/bg5.jpg'
+            , '../img/bg6.jpg'
+],
+        { duration: 4000, fade: 1000 })
+;
+            // var $boyCounter = $(".counter");
+            // if ($boyCounter.length) {
 
-                $.backstretch([
-                             '../assets/images/slider/1.jpg'
-                             , '../assets/images/slider/2.jpg'
-                             , '../assets/images/slider/3.jpg'
-                ],
-                         { duration: 4000, fade: 1000 })
-                ;
+            //     $.backstretch([
+            //                  '../assets/images/slider/1.jpg'
+            //                  , '../assets/images/slider/2.jpg'
+            //                  , '../assets/images/slider/3.jpg'
+            //     ],
+            //              { duration: 4000, fade: 1000 })
+            //     ;
 
-                //enter the last menstrual period date using the date format  year, month, day
-                $boyCounter.tictic({
-                    date: {
-                        year: 2017,
-                        month: 7,
-                        day: 9
-                    },
-                    charts: {
-                        disableAnimation: true,
-                        darkerColor: '#FF69B4',
-                        lighterColor: '#FF79B4'
-                    }
-                });
+            //     //enter the last menstrual period date using the date format  year, month, day
+            //     $boyCounter.tictic({
+            //         date: {
+            //             year: 2017,
+            //             month: 7,
+            //             day: 9
+            //         },
+            //         charts: {
+            //             disableAnimation: true,
+            //             darkerColor: '#FF69B4',
+            //             lighterColor: '#FF79B4'
+            //         }
+            //     });
 
-            }
+            // }
     });
 
 })(window.jQuery);
